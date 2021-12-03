63d6c1d223ba093b0e51ce33902f2a34f9082ac3
Tarcísio Teles <tfst@ecomp.poli.br>
2021-12-03 14:04:36 -0300

    lunch break

--- src/components/leftColumn/StoriesList.js
+++ src/components/leftColumn/StoriesList.js
@@ -1,16 +1,41 @@
 import Story from "./Story.js";
 
+const storiesArray = [
+    {
+        userName: "9gag",
+        userAvatar: "assets/images/9gag.svg"
+    },
+    {
+        userName: "meowed",
+        userAvatar: "assets/images/meowed.svg"
+    },
+    {
+        userName: "barked",
+        userAvatar: "assets/images/barked.svg"
+    },
+    {
+        userName: "nathanwpylestrangeplanet",
+        userAvatar: "assets/images/nathanwpylestrangeplanet.svg"
+    },
+    {
+        userName: "wawawiwacomics",
+        userAvatar: "assets/images/wawawicomics.svg"
+    },
+    {
+        userName: "respondeai",
+        userAvatar: "assets/images/respondeai.svg"
+    },
+    {
+        userName: "filomoderna",
+        userAvatar: "assets/images/filomoderna.svg"
+    },
+    {
+        userName: "memeriagourmet",
+        userAvatar: "assets/images/memeriagourmet.svg"
+    }
+];
+
 export default function StoriesList() {
-    const storiesArray = [
-        {
-            userName: "9gag",
-            userAvatar: "assets/images/9gag.svg"
-        },
-        {
-            userName: "barked",
-            userAvatar: "assets/images/barked.svg"
-        }
-    ];
     return (
         <ul className="stories-list">
             {storiesArray.map( story => 
--- src/components/leftColumn/Story.js
+++ src/components/leftColumn/Story.js
@@ -2,9 +2,11 @@ export default function Story(props) {
     
     return (
         <li className="story">
-            <p>{props.userName}</p>
-            <img className="story-bgframe" src="assets/images/stories_background.jpg" alt ="story background circle"/>
+            {/*
+                <img className="story-bgframe" src="assets/images/stories_background.jpg" alt ="story background circle"/>
+            */}
             <img className="who-avatar" src={props.userAvatar} alt="user avatar" />
+            <p>{props.userName}</p>
         </li>
     );
 }
