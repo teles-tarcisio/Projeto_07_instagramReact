ebb09f0fc57b61e05561fb357769792dd7ba6b71
Tarcísio Teles <tfst@ecomp.poli.br>
2021-12-03 15:17:35 -0300

    feat: splitting in more components

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,24 +1,36 @@
+function TopbarIcons() {
+    return (
+        <div className="top-bar_icons">
+            <ion-icon className="icon-paperplane" name="paper-plane-outline"></ion-icon>
+            <ion-icon className="icon-compass" name="compass-outline"></ion-icon>
+            <ion-icon className="icon-heart" name="heart-outline"></ion-icon>
+            <ion-icon className="icon-person" name="person-outline"></ion-icon>
+        </div>
+    );
+}
+
+function TopbarLogos() {
+    return (
+        <div className="top-bar_logos">
+            <ion-icon className="logo-insta" name="logo-instagram"></ion-icon>
+            <div className="logo-vert-spacer"></div>
+            <img className="lettering-insta" src="assets/images/logo.png" alt="instagram lettering logo" />
+        </div>
+
+    );
+}
+
+
 export default function Topbar() {
     return (
         <div classNameName="top-fixed-ribbon">
             <div classNameName="top-bar-content">
-
-                <div className="top-bar_logos">
-                    <ion-icon className="logo-insta" name="logo-instagram"></ion-icon>
-                    <div className="logo-vert-spacer"></div>
-                    <img className="lettering-insta" src="images/logo.png" alt="instagram lettering logo" />
-                </div>
+                <TopbarLogos />
 
                 <input type="text" placeholder="Pesquisar" />
 
-                <div className ="top-bar_icons">
-                <ion-icon className ="icon-paperplane" name="paper-plane-outline"></ion-icon>
-                <ion-icon className ="icon-compass" name="compass-outline"></ion-icon>
-                <ion-icon className ="icon-heart" name="heart-outline"></ion-icon>
-                <ion-icon className ="icon-person" name="person-outline"></ion-icon>
-                </div>
+                <TopbarIcons />
             </div>
-
         </div>
     )
 }
